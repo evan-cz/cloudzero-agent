e7cf65d9df12ebc9bca86d888480fb9dcedf0105
Daniel Mepham <daniel.mepham@cloudzero.com>
2024-10-04 14:02:09 -0400

    use vars

--- validation/snowflake_queries/03-summary-table/distinct-metrics-by-cluster.sql
+++ validation/snowflake_queries/03-summary-table/distinct-metrics-by-cluster.sql
@@ -1,10 +1,13 @@
+set organization_id = 'your-org-id';
+set lookback_time = '2024-10-01 10'::timestamp_tz;
+
 select
     distinct cluster_name,
     GET(labels, '__name__')::string as metric_name
 from
     public.prometheus_staging
 where
-    organization_id = '39bead45-5e64-48b0-94b5-0d349b5ca6ef'
-    and usage_date >= '2024-10-01 01'
+    organization_id = $organization_id
+    and usage_date >= $lookback_time
 order by
     cluster_name
