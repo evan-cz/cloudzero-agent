dbb3c0728036e7faab207b1c5b5bb97e2c879b8e
Daniel Mepham <daniel.mepham@cloudzero.com>
2025-01-17 15:03:11 -0500

    move to other file

--- tests/integration/config.go
+++ tests/integration/config.go
@@ -2,9 +2,14 @@ package integration
 
 // Constants for configuration values
 const (
+	TestOutputDir = "./test-output"
+)
+
+const (
 	BaseURL          = "http://localhost:8000"
 	ValidClusterName = "integration-test-cluster"
 	ValidAccountID   = "test-account-id"
 	ValidRegion      = "us-east-1"
-	ValidRoute       = "/v1/container-metrics"
+	PodRoute         = "/validate/pod"
+	HealthRoute      = "/healthz"
 )
--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -1,114 +1,87 @@
 package integration
 
 import (
-	"bytes"
-	"compress/gzip"
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/rs/zerolog/log"
-	v1 "k8s.io/api/admission/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
 )
 
-func generateRequest(method, url string, req Request) (*http.Request, error) {
-	query := "?"
-	for k, v := range req.QueryParams {
-		query += fmt.Sprintf("%s=%s&", k, v)
-	}
-	query = query[:len(query)-1]
-
-	bodyBytes, err := json.Marshal(req.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal body: %v", err)
-	}
-
-	var buf bytes.Buffer
-	gz := gzip.NewWriter(&buf)
-	if _, err := gz.Write(bodyBytes); err != nil {
-		return nil, fmt.Errorf("failed to compress body: %v", err)
-	}
-	if err := gz.Close(); err != nil {
-		return nil, fmt.Errorf("failed to close gzip writer: %v", err)
-	}
-
-	httpReq, err := http.NewRequest(method, url+query, &buf)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create request: %v", err)
-	}
-	httpReq.Header.Set("Content-Encoding", "gzip")
-
-	return httpReq, nil
-}
-
-func NewAdmissionRequest() []byte {
-	// Create an AdmissionRequest
-	admissionRequest := &v1.AdmissionRequest{
-		UID: types.UID("12345"),
-		Kind: metav1.GroupVersionKind{
-			Group:   "",
-			Version: "v1",
-			Kind:    "Pod",
-		},
-		Resource: metav1.GroupVersionResource{
-			Group:    "",
-			Version:  "v1",
-			Resource: "pods",
-		},
-		Name:      "example-pod",
-		Namespace: "default",
-		Operation: v1.Create,
-		Object: runtime.RawExtension{
-			Raw: []byte(`{
-                "apiVersion": "v1",
-                "kind": "Pod",
-                "metadata": {
-                    "name": "example-pod",
-                    "namespace": "default"
-                },
-                "spec": {
-                    "containers": [{
-                        "name": "example-container",
-                        "image": "example-image"
-                    }]
-                }
-            }`),
+func TestIntegrationValidResponses(t *testing.T) {
+	tests := []struct {
+		name           string
+		requests       []Request
+		route          string
+		expectedStatus int
+	}{
+		{
+			name: "Valid route request should return 200",
+			requests: []Request{
+				{QueryParams: map[string]string{
+					"cluster_name":     ValidClusterName,
+					"cloud_account_id": ValidAccountID,
+					"region":           ValidRegion,
+				}, Body: []byte{},
+					Route:  PodRoute,
+					Method: http.MethodPost,
+				},
+			},
+			expectedStatus: http.StatusOK,
 		},
 	}
 
-	// Create an AdmissionReview
-	admissionReview := &v1.AdmissionReview{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: "admission.k8s.io/v1",
-			Kind:       "AdmissionReview",
-		},
-		Request: admissionRequest,
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, req := range tt.requests {
+				log.Info().Msg("Running test")
+
+				admissionReq := NewAdmissionRequest()
+				if admissionReq == nil {
+					t.Fatalf("Failed to create test admission request")
+				}
+				httpReq, err := generateRequest(req.Method, req.Route, BaseURL, Request{Body: admissionReq, QueryParams: req.QueryParams})
+
+				if err != nil {
+					t.Fatalf("Failed to generate request: %v", err)
+				}
+
+				resp, err := http.DefaultClient.Do(httpReq)
+				if err != nil {
+					t.Fatalf("Failed to send request: %v", err)
+				}
+				defer resp.Body.Close()
+
+				if resp.StatusCode != tt.expectedStatus {
+					t.Errorf("Expected status %d, got %d", tt.expectedStatus, resp.StatusCode)
+				}
+
+				if resp.StatusCode == http.StatusOK {
+					body, err := io.ReadAll(resp.Body)
+					if err != nil {
+						t.Fatalf("Failed to read response body: %v", err)
+					}
+					t.Logf("Response body: %s", body)
+				}
+				time.Sleep(20 * time.Second)
+			}
+		})
 	}
-
-	// Marshal the AdmissionReview to JSON
-	admissionReviewJSON, err := json.Marshal(admissionReview)
-	if err != nil {
-		fmt.Printf("Error marshaling AdmissionReview: %v\n", err)
-		return nil
-	}
-	return admissionReviewJSON
-}
-
-type Request struct {
-	Method      string
-	QueryParams map[string]string
-	Body        []byte
-}
-
-type BodyParams struct {
-	Kind       string
-	UID        string
-	ObjectName string
+	t.Cleanup(func() {
+		files, err := os.ReadDir(TestOutputDir)
+		if err != nil {
+			t.Fatalf("Failed to read directory: %v", err)
+		}
+		for _, file := range files {
+			err := os.Remove(fmt.Sprintf("%s/%s", TestOutputDir, file.Name()))
+			if err != nil {
+				t.Fatalf("Failed to remove test file: %v", err)
+			}
+		}
+	})
 }
 
 func TestIntegrationInvalidResponses(t *testing.T) {
@@ -116,13 +89,16 @@ func TestIntegrationInvalidResponses(t *testing.T) {
 		name           string
 		requests       []Request
 		method         string
+		route          string
 		expectedStatus int
 	}{
 		{
 			name:   "Invalid route request should return 404",
 			method: http.MethodPost,
 			requests: []Request{
-				{QueryParams: map[string]string{}, Body: []byte{}},
+				{QueryParams: map[string]string{}, Body: []byte{},
+					Route: "/invalid-route",
+				},
 			},
 			expectedStatus: http.StatusNotFound,
 		},
@@ -138,7 +114,7 @@ func TestIntegrationInvalidResponses(t *testing.T) {
 					t.Fatalf("Failed to create fake request")
 				}
 				// Use a value from the config package
-				httpReq, err := generateRequest(req.Method, BaseURL, Request{Body: foo, QueryParams: req.QueryParams})
+				httpReq, err := generateRequest(req.Method, req.Route, BaseURL, Request{Body: foo, QueryParams: req.QueryParams})
 
 				if err != nil {
 					t.Fatalf("Failed to generate request: %v", err)
