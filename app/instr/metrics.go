f845b28ff10b6498a565613aec8fbb77a891e1ec
Jake Landers <jake.landers@cloudzero.com>
2025-02-04 15:05:22 -0800

    migrated back to prom, internal interface meant to be supplied with metrics from outside

--- app/instr/metrics.go
+++ app/instr/metrics.go
@@ -1,162 +1,107 @@
-// SPDX-FileCopyrightText: Copyright (c) 2016-2025, CloudZero, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 package instr
 
 import (
-	"context"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 	"sync"
-	"time"
 
-	prom "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/collectors"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/prometheus"
-	"go.opentelemetry.io/otel/metric"
-	api "go.opentelemetry.io/otel/sdk/metric"
 )
-
-/*
-Sources:
-- https://signoz.io/opentelemetry/go/
-- https://betterstack.com/community/guides/observability/opentelemetry-metrics-golang/
-- https://opentelemetry.io/docs/specs/otel/metrics/sdk_exporters/prometheus/
-- https://pkg.go.dev/go.opentelemetry.io/otel/exporters/prometheus
-- https://github.com/open-telemetry/opentelemetry-go-contrib/tree/main/examples/prometheus
-
-*/
-
-const OTEL_SCOPE_NAME = "github.com/Cloudzero/cloudzero-insights-controller"
-const OTEL_SCOPE_VERSION = "0.0.1-dev1"
 
 var (
-	initOnce sync.Once
-	provider *api.MeterProvider
-
-	// testing metric
-	testCounter metric.Int64Counter
-
-	// START - define metrics -------------
-
-	// the total number of requests we have posting to the remote write endpoint
-	RemoteWriteRequestCount metric.Int64Counter
-
-	// the total duration in seconds a remote write request takes
-	RemoteWriteRequestDurationSeconds metric.Float64Histogram
-
-	// status code counts broken down with whatever is labeled on metric post time
-	RemoteWriteStatusCodes metric.Int64Counter
-
-	// total size of a payload written to the remote write endpoint.
-	RemoteWritePayloadSizeBytes metric.Int64Histogram
-
-	// total number of failures on the remote write endpoint
-	RemoteWriteFailures metric.Int64Counter
+	registerOnce sync.Once
 )
 
-func init() {
-	// extra insurance that the particular init function is only ran ONCE
-	initOnce.Do(func() {
-		if err := _init(); err != nil {
-			log.Fatalf("Failed to setup otel metrics: %s", err.Error())
+type PrometheusMetrics struct {
+	registry    *prometheus.Registry
+	metrics     *[]prometheus.Collector
+	noGometrics bool
+}
+
+type PrometheusMetricsOpt func(*PrometheusMetrics)
+
+func WithPromMetrics(m ...prometheus.Collector) PrometheusMetricsOpt {
+	return func(p *PrometheusMetrics) {
+		p.metrics = &m
+	}
+}
+
+func WithCustomRegistry(registry *prometheus.Registry) PrometheusMetricsOpt {
+	return func(p *PrometheusMetrics) {
+		p.registry = registry
+	}
+}
+
+func WithDefaultRegistry() PrometheusMetricsOpt {
+	return func(p *PrometheusMetrics) {
+		p.registry = prometheus.DefaultRegisterer.(*prometheus.Registry)
+	}
+}
+
+func WithNoGoMetrics() PrometheusMetricsOpt {
+	return func(p *PrometheusMetrics) {
+		p.noGometrics = true
+	}
+}
+
+// Create a new prometheus metrics object. This will setup sane default prometheus
+// metrics, with additional configuration with `...PrometheusMetricsOpt`.
+func NewPrometheusMetrics(opts ...PrometheusMetricsOpt) (*PrometheusMetrics, error) {
+	p := &PrometheusMetrics{}
+
+	// apply the options
+	for _, item := range opts {
+		item(p)
+	}
+
+	// register the metrics
+	var registerErr error
+	registerOnce.Do(func() {
+		// apply a default internal registry if none set
+		if p.registry == nil {
+			p.registry = prometheus.NewRegistry()
+
+			// if using the default register, include the default go metrics as well if applicable
+			if !p.noGometrics {
+				p.registry.Register(collectors.NewGoCollector())
+				p.registry.Register(collectors.NewProcessCollector(collectors.ProcessCollectorOpts{}))
+			}
 		}
+
+		// register all the user-defined metrics
+		registerErr = p.register()
 	})
+	if registerErr != nil {
+		return nil, registerErr
+	}
+
+	return p, nil
 }
 
-// create the metric resources. When using the `prometheus` package, a meter provider lets
-// us use otel metrics while exposing to a prometheus-compatable write endpoint
-func _init() error {
-	var err error
-	exporter, err := prometheus.New()
-	if err != nil {
-		log.Fatal(err)
+// Get the http handler for this specific instance of the prometheus
+// metrics registry. If the `WithDefaultRegistry` option was used,
+// then calling `promhttp.Handler()` will return the same handler
+func (p *PrometheusMetrics) Handler() http.Handler {
+	return promhttp.HandlerFor(p.registry, promhttp.HandlerOpts{})
+}
+
+// Internal only. This registers the metrics WITHOUT BEING SAFE
+// this can cause panics if not used correctly.
+func (p *PrometheusMetrics) register() error {
+	for _, item := range *p.metrics {
+		if err := p.registry.Register(item); err != nil {
+			return fmt.Errorf("failed to register a metric: %w", err)
+		}
 	}
-	provider = api.NewMeterProvider(api.WithReader(exporter))
-	otel.SetMeterProvider(provider)
-
-	// read environment to get the scope name and version
-	name := os.Getenv("OTEL_SCOPE_NAME")
-	if name == "" {
-		name = OTEL_SCOPE_NAME
-	}
-	version := os.Getenv("OTEL_SCOPE_VERSION")
-	if version == "" {
-		version = OTEL_SCOPE_VERSION
-	}
-
-	// initialize the metrics we want to track
-	meter := provider.Meter(name, metric.WithInstrumentationVersion(version))
-
-	testCounter, err = meter.Int64Counter("test_counter")
-	if err != nil {
-		return err
-	}
-
-	// START - initialize all metrics -----------------
-
-	RemoteWriteRequestCount, err = meter.Int64Counter(
-		"remote_write_request_total",
-		metric.WithDescription("Total number of write attempts against the write endpoint"),
-	)
-	if err != nil {
-		return err
-	}
-
-	RemoteWriteRequestDurationSeconds, err = meter.Float64Histogram(
-		"remote_write_request_duration",
-		metric.WithDescription("Duration of requests to the remote write endpoint"),
-		metric.WithUnit("s"),
-		metric.WithExplicitBucketBoundaries(prom.DefBuckets...),
-	)
-	if err != nil {
-		return err
-	}
-
-	RemoteWriteStatusCodes, err = meter.Int64Counter(
-		"remote_write_status_codes_total",
-		metric.WithDescription("Count of response status codes from remote write endpoint"),
-	)
-	if err != nil {
-		return err
-	}
-
-	RemoteWritePayloadSizeBytes, err = meter.Int64Histogram(
-		"remote_write_payload_size_bytes",
-		metric.WithDescription("Payload size posted to the remote write endpoint"),
-		metric.WithUnit("byte"),
-		metric.WithExplicitBucketBoundaries(prom.ExponentialBuckets(256, 2, 10)...),
-	)
-	if err != nil {
-		return err
-	}
-
-	RemoteWriteFailures, err = meter.Int64Counter(
-		"remote_write_failure_total",
-		metric.WithDescription("Total number of failures for the remote write endpoint"),
-	)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
-// Must use this handler, as the metrics endpoint needs the `init` function to be called first
-func Handler() http.Handler {
-	return promhttp.Handler()
-}
-
-// Usually do not need to call this, but may prove useful in specific scenarios
-func Flush(ctx context.Context) error {
-	c, cancel := context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-
-	if err := provider.ForceFlush(c); err != nil {
-		return fmt.Errorf("error flushing metrics: %w", err)
+// Removes stored metrics from the registry. Calling this function on a metric not in the
+// registry will not cause a panic
+func (p *PrometheusMetrics) clearRegistry() {
+	for _, item := range *p.metrics {
+		p.registry.Unregister(item)
 	}
-	return nil
 }
--- app/instr/metrics_test.go
+++ app/instr/metrics_test.go
@@ -4,155 +4,331 @@
 package instr
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"strings"
+	"sync"
 	"testing"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/stretchr/testify/require"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/metric"
 )
 
-func TestPrometheusHandler(t *testing.T) {
-	// create a mock http service
-	srv := httptest.NewServer(Handler())
-	defer srv.Close()
+var (
+	testMetric = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "test_prom_metric",
+			Help: "used for running basic prometheus tests",
+		},
+		[]string{"name"},
+	)
+)
 
-	// record a sample metric
-	testCounter.Add(context.Background(), 10)
+// resets all of the sync counters
+func _testResentSync() {
+	registerOnce = sync.Once{}
+	httpMiddlewareStatsOnce = sync.Once{}
+	spanOnce = sync.Once{}
+}
 
-	// check the result
-	resp, err := http.Get(srv.URL)
+// send an http request expecting prometheus metrics back
+// and returns them as a string
+func _testSendPromRequest(t *testing.T, url string) string {
+	resp, err := http.Get(url)
 	require.NoError(t, err)
 	defer resp.Body.Close()
-
 	body, err := io.ReadAll(resp.Body)
 	require.NoError(t, err)
-	require.Contains(t, string(body), "test_counter")
-	require.Contains(t, string(body), "10")
+	return string(body)
 }
 
-func TestMetrics(t *testing.T) {
-	// create mock http server
-	srv := httptest.NewServer(Handler())
+func TestPrometheusMetricsRegistry(t *testing.T) {
+	defer _testResentSync()
+
+	// using internal registry
+	t.Run("TestInternalRegistry", func(t *testing.T) {
+		defer _testResentSync()
+		p, err := NewPrometheusMetrics(
+			WithPromMetrics(testMetric),
+		)
+		require.NoError(t, err)
+		defer p.clearRegistry()
+
+		srv := httptest.NewServer(p.Handler()) // internal handler
+		defer srv.Close()
+
+		testMetric.WithLabelValues("test").Inc()
+		body := _testSendPromRequest(t, srv.URL)
+		require.Contains(t, string(body), "test_prom_metric")
+	})
+
+	// using internal registry with default handler
+	t.Run("TestInternalRegistryDefaultHandler", func(t *testing.T) {
+		defer _testResentSync()
+		p, err := NewPrometheusMetrics(
+			WithPromMetrics(testMetric),
+		)
+		require.NoError(t, err)
+		defer p.clearRegistry()
+
+		srv := httptest.NewServer(promhttp.Handler()) // default handler
+		defer srv.Close()
+
+		testMetric.WithLabelValues("test").Inc()
+		body := _testSendPromRequest(t, srv.URL)
+		require.NotContains(t, string(body), "test_prom_metric")
+	})
+
+	// use default registry
+	t.Run("TestDefaultRegistry", func(t *testing.T) {
+		defer _testResentSync()
+		p, err := NewPrometheusMetrics(
+			WithDefaultRegistry(),
+			WithPromMetrics(testMetric),
+		)
+		require.NoError(t, err)
+		defer p.clearRegistry()
+
+		srv := httptest.NewServer(p.Handler()) // internal handler
+		defer srv.Close()
+
+		testMetric.WithLabelValues("test").Inc()
+		body := _testSendPromRequest(t, srv.URL)
+		require.Contains(t, string(body), "test_prom_metric")
+	})
+
+	// use default registry with default handler
+	t.Run("TestDefaultRegistryDefaultHandler", func(t *testing.T) {
+		defer _testResentSync()
+		p, err := NewPrometheusMetrics(
+			WithDefaultRegistry(),
+			WithPromMetrics(testMetric),
+		)
+		require.NoError(t, err)
+		defer p.clearRegistry()
+
+		srv := httptest.NewServer(promhttp.Handler()) // default handler
+		defer srv.Close()
+
+		testMetric.WithLabelValues("test").Inc()
+		body := _testSendPromRequest(t, srv.URL)
+		require.Contains(t, string(body), "test_prom_metric")
+	})
+}
+
+func TestPrometheusMetricsMiddleware(t *testing.T) {
+	defer _testResentSync()
+
+	// create the prom struct
+	p, err := NewPrometheusMetrics(
+		WithPromMetrics(testMetric),
+	)
+	require.NoError(t, err)
+
+	// prom handler
+	srv := httptest.NewServer(p.Handler())
 	defer srv.Close()
 
-	getBody := func(t *testing.T) string {
-		resp, err := http.Get(srv.URL)
-		require.NoError(t, err)
-		defer resp.Body.Close()
-		body, err := io.ReadAll(resp.Body)
-		require.NoError(t, err)
-		return string(body)
-	}
+	// send a test request using the middleware
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	rr := httptest.NewRecorder()
+	handler := p.Middleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(time.Second)
+		w.WriteHeader(http.StatusOK)
+	}))
+	handler.ServeHTTP(rr, req)
+	require.Equal(t, http.StatusOK, rr.Code)
 
-	t.Run("RemoteWriteRequestCount", func(t *testing.T) {
-		RemoteWriteRequestCount.Add(context.Background(), 1,
-			metric.WithAttributes(
-				attribute.String("endpoint", "https://test-endpoint-1.com"),
-			),
-		)
-
-		RemoteWriteRequestCount.Add(context.Background(), 2,
-			metric.WithAttributes(
-				attribute.String("endpoint", "https://test-endpoint-2.com"),
-			),
-		)
-
-		body := getBody(t)
-
-		// ensure that things are being recorded correctly
-		for _, line := range strings.Split(string(body), "\n") {
-			if strings.Contains(line, "https://test-endpoint-1.com") {
-				require.Contains(t, line, "1")
-			}
-			if strings.Contains(line, "https://test-endpoint-2.com") {
-				require.Contains(t, line, "2")
-			}
-		}
-	})
-
-	t.Run("RemoteWriteRequestDurationSeconds", func(t *testing.T) {
-		// add some sample ranges
-		var sum float64 = 0
-		for _, item := range []float64{0.02, 2.3, 10} {
-			sum += item
-			RemoteWriteRequestDurationSeconds.Record(context.Background(), item)
-		}
-
-		body := getBody(t)
-
-		// ensure they are grouped into buckets
-		for _, line := range strings.Split(string(body), "\n") {
-			if strings.Contains(line, "le=\"0.025\"") {
-				require.Contains(t, line, "1")
-			}
-			if strings.Contains(line, "le=\"5\"") {
-				require.Contains(t, line, "2")
-			}
-			if strings.Contains(line, "le=\"10\"") {
-				require.Contains(t, line, "3")
-			}
-			if strings.Contains(line, "remote_write_request_duration_count") {
-				require.Contains(t, line, "3")
-			}
-			if strings.Contains(line, "remote_write_request_duration_sum") {
-				require.Contains(t, line, fmt.Sprintf("%v", sum))
-			}
-		}
-	})
-
-	t.Run("RemoteWriteStatusCodes", func(t *testing.T) {
-		// add various statuses
-		statuses := []int{
-			http.StatusOK,
-			http.StatusInternalServerError,
-			http.StatusBadRequest,
-			http.StatusUnauthorized,
-		}
-		for _, item := range statuses {
-			RemoteWriteStatusCodes.Add(context.Background(), 1, metric.WithAttributes(attribute.Int("code", item)))
-		}
-		body := getBody(t)
-
-		// validate the body
-		seen := 0
-		for _, line := range strings.Split(body, "\n") {
-			if strings.Contains(line, "remote_write_status_codes_total{") {
-				seen += 1
-				require.Contains(t, line, "} 1")
-			}
-		}
-		require.Equal(t, 4, seen)
-	})
-
-	t.Run("RemoteWritePayloadSizeBytes", func(t *testing.T) {
-		sizes := []int64{2034, 34201, 20432, 93384}
-		var total int64 = 0
-		for _, item := range sizes {
-			total += item
-			RemoteWritePayloadSizeBytes.Record(context.Background(), item)
-		}
-		body := getBody(t)
-		fmt.Println(body)
-		for _, line := range strings.Split(body, "\n") {
-			if strings.Contains(line, "remote_write_payload_size_bytes_total{") {
-				require.Contains(t, line, fmt.Sprintf("%v", total))
-			}
-		}
-	})
-
-	t.Run("RemoteWriteFailures", func(t *testing.T) {
-		RemoteWriteFailures.Add(context.Background(), 1)
-		body := getBody(t)
-		for _, line := range strings.Split(body, "\n") {
-			if strings.Contains(line, "remote_write_failures_total{") {
-				require.Contains(t, line, "1")
-			}
-		}
-	})
+	// send a request to prometheus
+	body := _testSendPromRequest(t, srv.URL)
+	require.Contains(t, body, "http_request_duration_seconds_sum{")
+	require.Contains(t, body, "http_requests_total{")
 }
+
+func TestPrometheusMetricsSpan(t *testing.T) {
+	defer _testResentSync()
+
+	// create the prom struct
+	p, err := NewPrometheusMetrics(
+		WithPromMetrics(testMetric),
+		WithNoGoMetrics(),
+	)
+	require.NoError(t, err)
+
+	// prom handler
+	srv := httptest.NewServer(p.Handler())
+
+	// basic
+	p.Span("test_function_1", func() error {
+		time.Sleep(time.Second)
+		return nil
+	})
+
+	// with error
+	p.Span("test_function_2", func() error {
+		time.Sleep(time.Second / 2)
+		return fmt.Errorf("function failed")
+	})
+
+	body := _testSendPromRequest(t, srv.URL)
+	require.Contains(t, body, "function_name=\"test_function_1\"")
+	require.Contains(t, body, "function_name=\"test_function_2\"")
+	require.Contains(t, body, "error=\"function failed\"")
+}
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"io"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"strings"
+// 	"testing"
+
+// 	"github.com/stretchr/testify/require"
+// 	"go.opentelemetry.io/otel/attribute"
+// 	"go.opentelemetry.io/otel/metric"
+// )
+
+// func TestPrometheusHandler(t *testing.T) {
+// 	// create a mock http service
+// 	srv := httptest.NewServer(Handler())
+// 	defer srv.Close()
+
+// 	// record a sample metric
+// 	testCounter.Add(context.Background(), 10)
+
+// 	// check the result
+// 	resp, err := http.Get(srv.URL)
+// 	require.NoError(t, err)
+// 	defer resp.Body.Close()
+
+// 	body, err := io.ReadAll(resp.Body)
+// 	require.NoError(t, err)
+// 	require.Contains(t, string(body), "test_counter")
+// 	require.Contains(t, string(body), "10")
+// }
+
+// func TestMetrics(t *testing.T) {
+// 	// create mock http server
+// 	srv := httptest.NewServer(Handler())
+// 	defer srv.Close()
+
+// 	getBody := func(t *testing.T) string {
+// 		resp, err := http.Get(srv.URL)
+// 		require.NoError(t, err)
+// 		defer resp.Body.Close()
+// 		body, err := io.ReadAll(resp.Body)
+// 		require.NoError(t, err)
+// 		return string(body)
+// 	}
+
+// 	t.Run("RemoteWriteRequestCount", func(t *testing.T) {
+// 		RemoteWriteRequestCount.Add(context.Background(), 1,
+// 			metric.WithAttributes(
+// 				attribute.String("endpoint", "https://test-endpoint-1.com"),
+// 			),
+// 		)
+
+// 		RemoteWriteRequestCount.Add(context.Background(), 2,
+// 			metric.WithAttributes(
+// 				attribute.String("endpoint", "https://test-endpoint-2.com"),
+// 			),
+// 		)
+
+// 		body := getBody(t)
+
+// 		// ensure that things are being recorded correctly
+// 		for _, line := range strings.Split(string(body), "\n") {
+// 			if strings.Contains(line, "https://test-endpoint-1.com") {
+// 				require.Contains(t, line, "1")
+// 			}
+// 			if strings.Contains(line, "https://test-endpoint-2.com") {
+// 				require.Contains(t, line, "2")
+// 			}
+// 		}
+// 	})
+
+// 	t.Run("RemoteWriteRequestDurationSeconds", func(t *testing.T) {
+// 		// add some sample ranges
+// 		var sum float64 = 0
+// 		for _, item := range []float64{0.02, 2.3, 10} {
+// 			sum += item
+// 			RemoteWriteRequestDurationSeconds.Record(context.Background(), item)
+// 		}
+
+// 		body := getBody(t)
+
+// 		// ensure they are grouped into buckets
+// 		for _, line := range strings.Split(string(body), "\n") {
+// 			if strings.Contains(line, "le=\"0.025\"") {
+// 				require.Contains(t, line, "1")
+// 			}
+// 			if strings.Contains(line, "le=\"5\"") {
+// 				require.Contains(t, line, "2")
+// 			}
+// 			if strings.Contains(line, "le=\"10\"") {
+// 				require.Contains(t, line, "3")
+// 			}
+// 			if strings.Contains(line, "remote_write_request_duration_count") {
+// 				require.Contains(t, line, "3")
+// 			}
+// 			if strings.Contains(line, "remote_write_request_duration_sum") {
+// 				require.Contains(t, line, fmt.Sprintf("%v", sum))
+// 			}
+// 		}
+// 	})
+
+// 	t.Run("RemoteWriteStatusCodes", func(t *testing.T) {
+// 		// add various statuses
+// 		statuses := []int{
+// 			http.StatusOK,
+// 			http.StatusInternalServerError,
+// 			http.StatusBadRequest,
+// 			http.StatusUnauthorized,
+// 		}
+// 		for _, item := range statuses {
+// 			RemoteWriteStatusCodes.Add(context.Background(), 1, metric.WithAttributes(attribute.Int("code", item)))
+// 		}
+// 		body := getBody(t)
+
+// 		// validate the body
+// 		seen := 0
+// 		for _, line := range strings.Split(body, "\n") {
+// 			if strings.Contains(line, "remote_write_status_codes_total{") {
+// 				seen += 1
+// 				require.Contains(t, line, "} 1")
+// 			}
+// 		}
+// 		require.Equal(t, 4, seen)
+// 	})
+
+// 	t.Run("RemoteWritePayloadSizeBytes", func(t *testing.T) {
+// 		sizes := []int64{2034, 34201, 20432, 93384}
+// 		var total int64 = 0
+// 		for _, item := range sizes {
+// 			total += item
+// 			RemoteWritePayloadSizeBytes.Record(context.Background(), item)
+// 		}
+// 		body := getBody(t)
+// 		fmt.Println(body)
+// 		for _, line := range strings.Split(body, "\n") {
+// 			if strings.Contains(line, "remote_write_payload_size_bytes_total{") {
+// 				require.Contains(t, line, fmt.Sprintf("%v", total))
+// 			}
+// 		}
+// 	})
+
+// 	t.Run("RemoteWriteFailures", func(t *testing.T) {
+// 		RemoteWriteFailures.Add(context.Background(), 1)
+// 		body := getBody(t)
+// 		for _, line := range strings.Split(body, "\n") {
+// 			if strings.Contains(line, "remote_write_failures_total{") {
+// 				require.Contains(t, line, "1")
+// 			}
+// 		}
+// 	})
+// }
