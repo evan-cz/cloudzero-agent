6de202f748c6dfd645af6847982c27f5a9bc4638
Jake Landers <jake.landers@cloudzero.com>
2025-02-25 10:02:08 -0800

    small change to the sqlite connection stream, and added concurrency test

--- pkg/storage/sqlite/driver_test.go
+++ pkg/storage/sqlite/driver_test.go
@@ -4,14 +4,17 @@
 package sqlite_test
 
 import (
+	"database/sql"
+	"sync"
 	"testing"
 
 	"github.com/cloudzero/cloudzero-insights-controller/pkg/storage/sqlite"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gorm.io/gorm"
 )
 
-func TestNewSqlite3Driver(t *testing.T) {
+func TestSqlite3_NewDriver(t *testing.T) {
 	tests := []struct {
 		name    string
 		dsn     string
@@ -41,4 +44,53 @@ func TestNewSqlite3Driver(t *testing.T) {
 			}
 		})
 	}
+}
+
+// Based on a test from: https://gist.github.com/jmunson/7b1974215d34439688b0
+func TestSqlite3_ConcurrentAccess(t *testing.T) {
+	gormdb, err := sqlite.NewSQLiteDriver(sqlite.MemorySharedCached)
+	require.NoError(t, err, "failed to create sqlite driver")
+
+	db, err := gormdb.DB()
+	require.NoError(t, err, "failed to get underlying database")
+
+	_, err = db.Exec("CREATE TABLE 'test' (key TEXT, value TEXT)")
+	require.NoError(t, err, "failed to create test table")
+
+	_, err = db.Exec("INSERT into 'test' VALUES ('a', 'aye'), ('b', 'bee'), ('c', 'cee')")
+	require.NoError(t, err, "failed to isnert into test table")
+
+	//values that are in our db
+	kv := map[string]string{
+		"a": "aye",
+		"b": "bee",
+		"c": "cee",
+	}
+
+	var wg sync.WaitGroup
+
+	//We spawn 20 goroutines that each try to query every value in 'kv' and verify the results are correct
+	//Each goroutine is given a waitgroup and a thread number just to track the output.
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		go func(wg *sync.WaitGroup, thread int) {
+			defer wg.Done()
+			var errs int //number of failed results
+			for id, knownvalue := range kv {
+				resultvalue, err := lookup(db, id)
+				if err != nil || resultvalue != knownvalue {
+					t.Errorf("[%d] [Error:%s]  looking up [%s] got [%s] should be [%s]", thread, err, id, resultvalue, knownvalue)
+					errs++
+				}
+			}
+		}(&wg, i)
+	}
+
+	wg.Wait()
+}
+
+func lookup(db *sql.DB, key string) (string, error) {
+	var value string
+	err := db.QueryRow("SELECT value FROM 'test' where key=?", key).Scan(&value)
+	return value, err
 }
