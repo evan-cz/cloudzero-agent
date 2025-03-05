05d9f1e4c806c4ac01b1b28e254466121ff83b11
Jake Landers <jake.landers@cloudzero.com>
2025-03-04 17:13:49 -0800

    some re-org

--- app/domain/shipper/replay.go
+++ app/domain/shipper/replay.go
@@ -6,11 +6,15 @@ package shipper
 import (
 	"encoding/json"
 	"fmt"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
+	"github.com/cloudzero/cloudzero-insights-controller/app/lock"
 	"github.com/go-obvious/timestamp"
+	"github.com/rs/zerolog/log"
 )
 
 type ReplayRequest struct {
@@ -109,4 +113,169 @@ func (m *MetricShipper) GetActiveReplayRequests() ([]*ReplayRequest, error) {
 	}
 
 	return requests, nil
+}
+
+func (m *MetricShipper) ProcessReplayRequests() error {
+	log.Ctx(m.ctx).Info().Msg("Processing replay requests")
+
+	// ensure the directory is created
+	if err := os.MkdirAll(m.GetReplayRequestDir(), filePermissions); err != nil {
+		return fmt.Errorf("failed to create the replay request file directory: %w", err)
+	}
+
+	// lock the replay request dir for the duration of the replay request processing
+	log.Ctx(m.ctx).Debug().Msg("Aquiring file lock")
+	l := lock.NewFileLock(
+		m.ctx, filepath.Join(m.GetReplayRequestDir(), ".lock"),
+		lock.WithStaleTimeout(time.Second*30), // detects stale timeout
+		lock.WithRefreshInterval(time.Second*5),
+		lock.WithMaxRetry(lockMaxRetry), // 5 min wait
+	)
+	if err := l.Acquire(); err != nil {
+		return fmt.Errorf("failed to aquire the lock: %w", err)
+	}
+	defer func() {
+		if err := l.Release(); err != nil {
+			log.Ctx(m.ctx).Error().Err(err).Msg("Failed to release the lock")
+		}
+	}()
+
+	// read all valid replay request files
+	requests, err := m.GetActiveReplayRequests()
+	if err != nil {
+		return fmt.Errorf("failed to get replay requests: %w", err)
+	}
+
+	if len(requests) == 0 {
+		log.Ctx(m.ctx).Info().Msg("No replay requests found, skipping")
+		return nil
+	}
+
+	// handle all valid replay requests
+	for _, rr := range requests {
+		metricReplayRequestFileCount.Observe(float64(len(rr.ReferenceIDs)))
+
+		if err := m.HandleReplayRequest(rr); err != nil {
+			metricReplayRequestErrorTotal.WithLabelValues(err.Error()).Inc()
+			return fmt.Errorf("failed to process replay request '%s': %w", rr.Filepath, err)
+		}
+
+		// decrease the current queue for this replay request
+		metricReplayRequestCurrent.WithLabelValues().Dec()
+	}
+
+	log.Ctx(m.ctx).Info().Msg("Successfully handled all replay requests")
+
+	return nil
+}
+
+func (m *MetricShipper) HandleReplayRequest(rr *ReplayRequest) error {
+	log.Ctx(m.ctx).Debug().Str("rr", rr.Filepath).Int("numfiles", len(rr.ReferenceIDs)).Msg("Handling replay request")
+
+	// compose loopup map of reference ids
+	refIDLookup := make(map[string]any)
+	for _, item := range rr.ReferenceIDs {
+		refIDLookup[item] = struct{}{}
+	}
+
+	// fetch the new files that match these ids
+	newFiles := make([]string, 0)
+	if err := m.lister.Walk("", func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// skip dir
+		if info.IsDir() {
+			return nil
+		}
+
+		// check for a match
+		if _, exists := refIDLookup[info.Name()]; exists {
+			newFiles = append(newFiles, info.Name())
+		}
+
+		return nil
+	}); err != nil {
+		return fmt.Errorf("failed to get matching new files: %w", err)
+	}
+	log.Ctx(m.ctx).Debug().Int("newFiles", len(newFiles)).Send()
+
+	// fetch the already uploadedFiles files that match these ids
+	uploadedFiles := make([]string, 0)
+	if err := m.lister.Walk(UploadedSubDirectory, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// skip dir
+		if info.IsDir() {
+			return nil
+		}
+
+		// check for a match
+		if _, exists := refIDLookup[info.Name()]; exists {
+			uploadedFiles = append(uploadedFiles, info.Name())
+		}
+
+		return nil
+	}); err != nil {
+		return fmt.Errorf("failed to get matching uploaded files: %w", err)
+	}
+	log.Ctx(m.ctx).Debug().Int("uploadedFiles", len(uploadedFiles)).Send()
+
+	// combine found ids into a map
+	found := make(map[string]*MetricFile) // {ReferenceID: File}
+	for _, item := range newFiles {
+		file, err := NewMetricFile(filepath.Join(m.GetBaseDir(), filepath.Base(item)))
+		if err != nil {
+			return fmt.Errorf("failed to create file: %w", err)
+		}
+		found[filepath.Base(item)] = file
+	}
+	for _, item := range uploadedFiles {
+		file, err := NewMetricFile(filepath.Join(m.GetUploadedDir(), filepath.Base(item)))
+		if err != nil {
+			return fmt.Errorf("failed to create file: %w", err)
+		}
+		found[filepath.Base(item)] = file
+	}
+	log.Ctx(m.ctx).Debug().Int("found", len(found)).Send()
+
+	// compare the results and discover which files were not found
+	missing := make([]string, 0)
+	valid := make([]*MetricFile, 0)
+	for _, item := range rr.ReferenceIDs {
+		file, exists := found[filepath.Base(item)]
+		if exists {
+			valid = append(valid, file)
+		} else {
+			missing = append(missing, filepath.Base(item))
+		}
+	}
+
+	log.Info().Msgf("Replay request '%s': %d/%d files found", rr.Filepath, len(valid), len(rr.ReferenceIDs))
+
+	// send abandon requests for the non-found files
+	if len(missing) > 0 {
+		log.Info().Msgf("Replay request '%s': %d files missing, sending abandon request for these files", rr.Filepath, len(missing))
+		if err := m.AbandonFiles(missing, "not found"); err != nil {
+			metricReplayRequestAbandonFilesErrorTotal.WithLabelValues(err.Error()).Inc()
+			return fmt.Errorf("failed to send the abandon file request: %w", err)
+		}
+	}
+
+	// run the `HandleRequest` function for these files
+	if err := m.HandleRequest(valid); err != nil {
+		return fmt.Errorf("failed to upload replay request files: %w", err)
+	}
+
+	// delete the replay request
+	if err := os.Remove(rr.Filepath); err != nil {
+		return fmt.Errorf("failed to delete the replay request file: %w", err)
+	}
+
+	log.Ctx(m.ctx).Info().Str("rr", rr.Filepath).Msg("Successfully handled replay request")
+
+	return nil
 }
--- app/domain/shipper/upload.go
+++ app/domain/shipper/upload.go
@@ -3,7 +3,6 @@ package shipper
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -20,11 +19,6 @@ type PresignedURLAPIPayloadFile struct {
 	ReferenceID string `json:"reference_id"`      //nolint:tagliatelle // downstream expects cammel case
 	SHA256      string `json:"sha_256,omitempty"` //nolint:tagliatelle // downstream expects cammel case
 	Size        int64  `json:"size,omitempty"`
-}
-
-type AbandonAPIPayloadFile struct {
-	ReferenceID string `json:"reference_id"` //nolint:tagliatelle // downstream expects cammel case
-	Reason      string `json:"reason"`
 }
 
 // Allocates a set of pre-signed urls for the passed file objects
@@ -150,80 +144,4 @@ func (m *MetricShipper) AllocatePresignedURLs(files []*MetricFile) ([]*MetricFil
 	}
 
 	return files, nil
-}
-
-// sends an abandon request for a list of files with a given reason
-func (m *MetricShipper) AbandonFiles(referenceIDs []string, reason string) error {
-	if len(referenceIDs) == 0 {
-		return errors.New("cannot send in an empty slice")
-	}
-
-	// create the body
-	body := make([]*AbandonAPIPayloadFile, len(referenceIDs))
-	for i, item := range referenceIDs {
-		body[i] = &AbandonAPIPayloadFile{
-			ReferenceID: item,
-			Reason:      reason,
-		}
-	}
-
-	// serialize the body
-	enc, err := json.Marshal(body)
-	if err != nil {
-		return fmt.Errorf("failed to encode the body: %w", err)
-	}
-
-	// Create a new HTTP request
-	abandonEndpoint, err := m.setting.GetRemoteAPIBase()
-	if err != nil {
-		return fmt.Errorf("failed to get the abandon endpoint: %w", err)
-	}
-	abandonEndpoint.Path += "/abandon"
-	req, err := http.NewRequestWithContext(m.ctx, "POST", abandonEndpoint.String(), bytes.NewBuffer(enc))
-	if err != nil {
-		return fmt.Errorf("failed to create HTTP request: %w", err)
-	}
-
-	// Set necessary headers
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", m.setting.GetAPIKey())
-
-	// Make sure we set the query parameters for count, cloud_account_id, region, cluster_name
-	q := req.URL.Query()
-	q.Add("count", strconv.Itoa(len(referenceIDs)))
-	q.Add("cluster_name", m.setting.ClusterName)
-	q.Add("cloud_account_id", m.setting.CloudAccountID)
-	q.Add("region", m.setting.Region)
-	req.URL.RawQuery = q.Encode()
-
-	log.Info().Msgf("Abandoning %d files from '%s'", len(referenceIDs), req.URL.String())
-
-	// Send the request
-	resp, err := m.HTTPClient.Do(req)
-	if err != nil {
-		log.Error().Err(err).Msg("HTTP request failed")
-		return fmt.Errorf("HTTP request failed: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusUnauthorized || resp.StatusCode == http.StatusForbidden {
-		return ErrUnauthorized
-	}
-
-	// Check for HTTP errors
-	if resp.StatusCode != http.StatusOK {
-		bodyBytes, _ := io.ReadAll(resp.Body)
-		return fmt.Errorf("unexpected status code %d: %s", resp.StatusCode, string(bodyBytes))
-	}
-
-	// log the number of success abandoned files
-	counter, err := metricReplayRequestAbandonFilesTotal.GetMetricWithLabelValues()
-	if err != nil {
-		log.Ctx(m.ctx).Err(err).Msg("failed to get the prometheus metric")
-	} else {
-		counter.Add(float64(len(referenceIDs)))
-	}
-
-	// success
-	return nil
 }
