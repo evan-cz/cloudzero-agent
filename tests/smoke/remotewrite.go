d3433b7c39418ed6dacf272c95fb3d860604d8fc
jake-cloudzero <jake.landers@cloudzero.com>
2025-04-02 09:09:25 -0700

    remote go routine for building remotewrite

--- tests/smoke/remotewrite.go
+++ tests/smoke/remotewrite.go
@@ -7,10 +7,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"sync"
 	"time"
 
-	"github.com/cloudzero/cloudzero-agent-validator/app/config/gator"
+	config "github.com/cloudzero/cloudzero-agent-validator/app/config/gator"
 	remotewrite "github.com/cloudzero/cloudzero-agent-validator/mock/remotewrite/pkg"
 	"github.com/cloudzero/cloudzero-agent-validator/tests/utils"
 	"github.com/stretchr/testify/require"
@@ -21,97 +20,85 @@ import (
 func (t *testContext) StartMockRemoteWrite() *testcontainers.Container {
 	t.CreateNetwork()
 
-	var wg sync.WaitGroup
-
 	if t.s3instance == nil {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			fmt.Println("Creating the mock s3 instance ...")
+		fmt.Println("Creating the mock s3 instance ...")
 
-			s3instanceRequest := testcontainers.ContainerRequest{
-				Image:    "minio/minio:latest",
-				Networks: []string{t.network.Name},
-				Name:     t.s3instanceName,
-				Env: map[string]string{
-					"MINIO_ROOT_USER":     "minio-admin",
-					"MINIO_ROOT_PASSWORD": "minio-admin",
-				},
-				Cmd:             []string{"server", "/data"},
-				WaitingFor:      wait.ForLog("API: http://").WithStartupTimeout(2 * time.Minute),
-				AutoRemove:      true,
-				AlwaysPullImage: true,
-				LogConsumerCfg: &testcontainers.LogConsumerConfig{
-					Consumers: []testcontainers.LogConsumer{&stdoutLogConsumer{}},
-				},
-			}
+		s3instanceRequest := testcontainers.ContainerRequest{
+			Image:    "minio/minio:latest",
+			Networks: []string{t.network.Name},
+			Name:     t.s3instanceName,
+			Env: map[string]string{
+				"MINIO_ROOT_USER":     "minio-admin",
+				"MINIO_ROOT_PASSWORD": "minio-admin",
+			},
+			Cmd:             []string{"server", "/data"},
+			WaitingFor:      wait.ForLog("API: http://").WithStartupTimeout(2 * time.Minute),
+			AutoRemove:      true,
+			AlwaysPullImage: true,
+			LogConsumerCfg: &testcontainers.LogConsumerConfig{
+				Consumers: []testcontainers.LogConsumer{&stdoutLogConsumer{}},
+			},
+		}
 
-			s3instance, err := testcontainers.GenericContainer(t.ctx, testcontainers.GenericContainerRequest{
-				ContainerRequest: s3instanceRequest,
-				Started:          true,
-			})
-			require.NoError(t, err, "failed to create the s3 instance")
-			t.s3instance = &s3instance
+		s3instance, err := testcontainers.GenericContainer(t.ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: s3instanceRequest,
+			Started:          true,
+		})
+		require.NoError(t, err, "failed to create the s3 instance")
+		t.s3instance = &s3instance
 
-			fmt.Println("Successfully created the s3 instance")
-		}()
+		fmt.Println("Successfully created the s3 instance")
 	}
 
 	if t.remotewrite == nil {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			fmt.Println("Building the mock remotewrite ...")
+		fmt.Println("Building the mock remotewrite ...")
 
-			s3Endpoint := fmt.Sprintf("%s:9000", t.s3instanceName)
-			fmt.Printf("With mock s3 endpoint: '%s'\n", s3Endpoint)
+		s3Endpoint := fmt.Sprintf("%s:9000", t.s3instanceName)
+		fmt.Printf("With mock s3 endpoint: '%s'\n", s3Endpoint)
 
-			remotewriteReq := testcontainers.ContainerRequest{
-				FromDockerfile: testcontainers.FromDockerfile{
-					Context:    "../..",
-					Dockerfile: "tests/docker/Dockerfile.remotewrite",
-					KeepImage:  true,
-				},
-				Name:         t.remotewriteName,
-				Networks:     []string{t.network.Name},
-				ExposedPorts: []string{t.remoteWritePort},
-				Entrypoint:   []string{"/app/remotewrite"},
-				Env: map[string]string{
-					"API_KEY": t.apiKey,
-					"PORT":    t.remoteWritePort,
+		remotewriteReq := testcontainers.ContainerRequest{
+			FromDockerfile: testcontainers.FromDockerfile{
+				Context:    "../..",
+				Dockerfile: "tests/docker/Dockerfile.remotewrite",
+				KeepImage:  true,
+			},
+			Name:         t.remotewriteName,
+			Networks:     []string{t.network.Name},
+			ExposedPorts: []string{t.remoteWritePort},
+			Entrypoint:   []string{"/app/remotewrite"},
+			Env: map[string]string{
+				"API_KEY": t.apiKey,
+				"PORT":    t.remoteWritePort,
 
-					// minio creds
-					"S3_ENDPOINT":    s3Endpoint,
-					"S3_ACCESS_KEY":  "minio-admin",
-					"S3_PRIVATE_KEY": "minio-admin",
+				// minio creds
+				"S3_ENDPOINT":    s3Endpoint,
+				"S3_ACCESS_KEY":  "minio-admin",
+				"S3_PRIVATE_KEY": "minio-admin",
 
-					// mock delays
-					"UPLOAD_DELAY_MS": t.uploadDelayMs,
-				},
-				LogConsumerCfg: &testcontainers.LogConsumerConfig{
-					Consumers: []testcontainers.LogConsumer{&stdoutLogConsumer{}},
-				},
-				WaitingFor: wait.ForLog("Mock remotewrite is listening on: 'localhost:"),
-			}
+				// mock delays
+				"UPLOAD_DELAY_MS": t.uploadDelayMs,
+			},
+			LogConsumerCfg: &testcontainers.LogConsumerConfig{
+				Consumers: []testcontainers.LogConsumer{&stdoutLogConsumer{}},
+			},
+			WaitingFor: wait.ForLog("Mock remotewrite is listening on: 'localhost:"),
+		}
 
-			remotewrite, err := testcontainers.GenericContainer(t.ctx, testcontainers.GenericContainerRequest{
-				ContainerRequest: remotewriteReq,
-				Started:          true,
-			})
-			require.NoError(t, err, "failed to create the mock remotewrite")
+		remotewrite, err := testcontainers.GenericContainer(t.ctx, testcontainers.GenericContainerRequest{
+			ContainerRequest: remotewriteReq,
+			Started:          true,
+		})
+		require.NoError(t, err, "failed to create the mock remotewrite")
 
-			fmt.Println("Mock remotewrite built successfully")
-			t.remotewrite = &remotewrite
+		fmt.Println("Mock remotewrite built successfully")
+		t.remotewrite = &remotewrite
 
-			// set the host as the setting
-			t.SetSettings(func(settings *config.Settings) error {
-				settings.Cloudzero.Host = fmt.Sprintf("%s:%s", t.remotewriteName, t.remoteWritePort)
-				return nil
-			})
-		}()
+		// set the host as the setting
+		t.SetSettings(func(settings *config.Settings) error {
+			settings.Cloudzero.Host = fmt.Sprintf("%s:%s", t.remotewriteName, t.remoteWritePort)
+			return nil
+		})
 	}
-
-	wg.Wait()
 
 	return t.remotewrite
 }
