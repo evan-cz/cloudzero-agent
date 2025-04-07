9623a5dfc2c673ac869e11a28198ef95e9011a2f
jake-cloudzero <jake.landers@cloudzero.com>
2025-04-07 11:33:48 -0700

    remove old reference to inspector in pkg

--- cmd/agent-inspector/main.go
+++ cmd/agent-inspector/main.go
@@ -12,7 +12,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/cloudzero/cloudzero-agent-validator/pkg/inspector"
+	"github.com/cloudzero/cloudzero-agent-validator/app/inspector"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
